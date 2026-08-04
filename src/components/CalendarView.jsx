efff2e215e7bf9d776838cfb19c49e5954ea4bc5
seo401b <tjtkddnjs880@naver.com>
2026-08-04 13:04:30 +0000

    달력 기본 뼈대

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -2,12 +2,35 @@
 import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './CalendarView.css';
 
 function CalendarView({ date, onDateChange }) {
+  
+
+  const renderTileContent = ({ date, view }) => {
+    if (view === 'month') {
+      return (
+        <div className="day-split-container">
+          <div className="sub-cell cell-1"></div>
+          <div className="sub-cell cell-2"></div>
+          <div className="sub-cell cell-3"></div>
+          <div className="sub-cell cell-4"></div>
+          <div className="sub-cell cell-5"></div>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="calendar-container">
       <h2>달력 화면</h2>
-      <Calendar onChange={onDateChange} value={date} locale="ko-KR" />
+      <Calendar 
+        onChange={onDateChange} 
+        value={date} 
+        locale="ko-KR" 
+        tileContent={renderTileContent} // 여기에 tileContent 주입!
+      />
     </div>
   );
 }
